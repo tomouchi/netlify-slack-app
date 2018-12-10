07c1aefa54da271cc839c98dca33ee2945bb4977
tomouchi <wisteria-0004@m3.gyao.ne.jp>
2018-12-10 10:15:18 +0900

    Update events.js

--- src/events.js
+++ src/events.js
@@ -5,8 +5,10 @@ exports.handler = async (event, context, callback) => {
   const body = JSON.parse(event.body)
   const slackEvent = body.event
   console.log(JSON.stringify(body, null, 4))
+  console.log('aaaaaaaa')
 
   if (slackEvent && slackEvent.type === 'reaction_added' && slackEvent.item.type === 'message') {
+      console.log('bbbbbbbb')
     const web = new WebClient(process.env.SLACK_TOKEN)
     const res = await web.conversations.history({
       latest: slackEvent.item.ts,
@@ -14,6 +16,7 @@ exports.handler = async (event, context, callback) => {
       channel: slackEvent.item.channel,
       inclusive: true
     })
+    console.log('ooooooooooooooo')
     const message = res.messages[0]
     const octokit = Octokit()
     octokit.authenticate({
@@ -22,7 +25,7 @@ exports.handler = async (event, context, callback) => {
     })
 
     const content = (new Buffer(message.text)).toString('base64')
-
+    console.log('pppppppppppppppppppp')
     octokit.repos.createFile({
       owner: 'mottox2',
       repo: 'netlify-slack-app',
@@ -31,7 +34,9 @@ exports.handler = async (event, context, callback) => {
       content
     })
     
+    
     console.log(JSON.stringify(message, null, 4))
+    console.log('zzzzzzzzz')
   }
 
   callback(null, {
