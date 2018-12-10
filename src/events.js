25696a3e7ce6df16e43053c296dd3df9330aa2e2
tomokazuUchiyama <wisteria-0004@m3.gyao.ne.jp>
2018-12-10 23:34:19 +0900

    Revert "Update events.js"
    This reverts commit 07c1aefa

--- src/events.js
+++ src/events.js
@@ -1,27 +1,27 @@
-import { WebClient } from '@slack/client'
-import Octokit from '@octokit/rest'
+import {WebClient} from '@slack/client';
+import Octokit from '@octokit/rest';
 
 exports.handler = async (event, context, callback) => {
-  const body = JSON.parse(event.body)
-  const slackEvent = body.event
-  console.log(JSON.stringify(body, null, 4))
+  const body = JSON.parse(event.body);
+  const slackEvent = body.event;
+  console.log(JSON.stringify(body, null, 4));
 
   if (slackEvent && slackEvent.type === 'reaction_added' && slackEvent.item.type === 'message') {
-    const web = new WebClient(process.env.SLACK_TOKEN)
+    const web = new WebClient(process.env.SLACK_TOKEN);
     const res = await web.conversations.history({
       latest: slackEvent.item.ts,
       limit: 1,
       channel: slackEvent.item.channel,
       inclusive: true
-    })
-    const message = res.messages[0]
-    const octokit = Octokit()
+    });
+    const message = res.messages[0];
+    const octokit = Octokit();
     octokit.authenticate({
       type: 'oauth',
       token: process.env.GITHUB_TOKEN
-    })
+    });
 
-    const content = (new Buffer(message.text)).toString('base64')
+    const content = (new Buffer(message.text)).toString('base64');
 
     octokit.repos.createFile({
       owner: 'tomouchi',
@@ -29,13 +29,13 @@ exports.handler = async (event, context, callback) => {
       path: `data/${slackEvent.item.ts}.txt`,
       message: 'Added by netlify-slack-app',
       content
-    })
-    
-    console.log(JSON.stringify(message, null, 4))
+    });
+
+    console.log(JSON.stringify(message, null, 4));
   }
 
   callback(null, {
     statusCode: 200,
     body: body.challenge
-  })
-}
+  });
+};
